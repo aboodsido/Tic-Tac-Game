4ef24cf1b8b632e536268a254da9420018666ecd
aboodsido <odod41907@gmail.com>
2022-08-11 14:31:03 +0300

    first commit

---
+++ xo_game/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF00061a),
+        shadowColor: Color(0xFF001456),
+        splashColor: Color(0xFF4169e8),
+      ),
+      home:  const HomePage(),
+    );
+  }
+}
